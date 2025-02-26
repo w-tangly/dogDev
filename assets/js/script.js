0b1a700414e40a9739b863135db2653483cc04f1
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2025-02-26 18:51:58 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,46 @@ let testemonials = [
     {quote:'"DogDev usa ingredientes simples e limpos em seus produtos."', origin:'fox.svg'}, 
     {quote:'"Vejo a DogDev como uma verdadeira heroína do ramo"', origin:'sharktank.svg'}
 ]
+
+//captura os elementos do html usando queryselector
+let testemonialQuote=document.querySelector('.testemonials .quote')
+let testemonialIcons=document.querySelector('.testemonials .icons')
+
+//repetição para criar as imagens
+for(let tindex in testemonials){
+    let img=document.createElement('img')
+    img.setAttribute('src', './assets/images/'+testemonials[parseInt(tindex)].origin3)
+    img.style.cursor='pointer'
+    img.addEventListener('click', ()=>fillTestemonials(parseInt(tindex)))
+    testemonialIcons.appendChild(img)
+}
+
+// variavel para controlar o testemunho atual
+let currentTestemonial=0
+
+//variavel para controlar o tempo de cada testemunho
+let testemonialTimer
+
+//funcao para preencher o testemunho
+const fillTestemonials = (index) =>{
+    clearTimeout(testemonialTimer)
+    currentTestemonial=index
+    //acessa o item quote dentro dentro do vetor de testemunhos
+    testemonialQuote.innerHTML=testemonials[currentTestemonial].quote
+    let imgs = testemonialsIcons.querySelectorAll('img')
+    for(let img of imgs) img.style.opacity=0.2
+    imgs[currentTestemonial].style.opacity=1
+    testemonialTimer = setTimeout(nextTestemonial, 3000)
+}
+
+//funcao que passa para o proximo testemunho
+const nextTestemonial=()=>{
+    if(testemonials[currentTestemonial+1]){
+        fillTestemonials(currentTestemonial+1)
+    }else{
+        fillTestemonials(0)
+    }
+}
+
+//inicia a exibição dos testemunhos
+nextTestemonial()
