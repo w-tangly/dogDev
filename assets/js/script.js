e4b7c6b3f1f06f04d48770c1649b13ff28490c60
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2025-02-26 15:25:03 +0000

    Section testemonials

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,12 @@ menuOpener.addEventListener('click', ()=>{
         menuOpener.querySelector('.hamburger-icon') .style .display='none'
     }
 })
+
+// TESTEMONIALS
+// Vetor de objetos contendo as frases e os icones
+let testemonials = [
+    {quote:'"Mais do que nunca, os animais são tratados como membros da família."', origin:'cbs.svg'}, 
+    {quote:'"DogDev é um serviço de entrega direto ao consumidor, preparado na hora com ingredientes 100% reais. Ingredientes que humanos reconheceriam."', origin:'forbes.svg'}, 
+    {quote:'"DogDev usa ingredientes simples e limpos em seus produtos."', origin:'fox.svg'}, 
+    {quote:'"Vejo a DogDev como uma verdadeira heroína do ramo"', origin:'sharktank.svg'}
+]
